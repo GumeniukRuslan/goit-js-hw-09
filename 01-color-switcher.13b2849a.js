7309ad5a98baae6f4e8a7ca2d5664f25bbfab157
GumeniukRuslan <117035542+GumeniukRuslan@users.noreply.github.com>
2023-04-04 15:14:53 +0000

    Deploying to gh-pages from @ GumeniukRuslan/goit-js-hw-09@1774f959a481feae0d98e063d63d1fd3b3b1204d 🚀

---
+++ 01-color-switcher.13b2849a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o=null;t.addEventListener("click",(function(){t.setAttribute("disabled",""),e.removeAttribute("disabled"),o=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){e.setAttribute("disabled",""),t.removeAttribute("disabled"),clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.13b2849a.js.map
